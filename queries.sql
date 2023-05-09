1f15541e99c72c133f097dbc991e4cc8e48c7fbf
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-05-09 15:35:59 -0600

    updated queries and images

--- queries.sql
+++ queries.sql
@@ -36,3 +36,7 @@ SELECT * from animals ;
 COMMIT;
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals where escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, AVG(escape_attempts) AS avg_escape_attempts FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01'AND '2000-12-31' GROUP BY species;
