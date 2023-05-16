2f3fbff0c911e5424ba97d000738e1a62e81ff4d
Santiago <santi.ponsle@gmail.com>
2023-05-16 17:27:09 -0300

    Add Invoice and treatments tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -8,5 +8,28 @@ CREATE TABLE medical_histories (
     id SERIAL PRIMARY KEY,
     admitted_at TIMESTAMP,
     FOREIGN KEY patient_id INT REFERENCES patients(id),
-    status VARCHAR(80),
+    status VARCHAR(80)
+);
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL(10,2) NOT NULL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    FOREIGN KEY medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatments (
+    FOREIGN KEY id INT REFERENCES medical_histories(id),
+    type VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL(10,2),
+    FOREIGN KEY invoice_id INT REFERENCES invoices(id),
+    FOREIGN KEY treatment_id INT REFERENCES treatments(id)
 );
