4b7a032ddbacf0c6df1933866217b9809f6d8e63
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-05-16 15:14:45 -0600

    Fix errors

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,6 +11,8 @@ CREATE TABLE medical_histories (
     status VARCHAR(80)
 );
 
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
+
 CREATE TABLE invoices (
     id SERIAL PRIMARY KEY,
     total_amount DECIMAL(10,2) NOT NULL,
@@ -19,8 +21,10 @@ CREATE TABLE invoices (
     FOREIGN KEY medical_history_id INT REFERENCES medical_histories(id)
 );
 
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+
 CREATE TABLE treatments (
-    FOREIGN KEY id INT REFERENCES medical_histories(id),
+    id INT PRIMARY KEY REFERENCES medical_histories(id),
     type VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL
 );
@@ -32,4 +36,13 @@ CREATE TABLE invoice_items (
     total_price DECIMAL(10,2),
     FOREIGN KEY invoice_id INT REFERENCES invoices(id),
     FOREIGN KEY treatment_id INT REFERENCES treatments(id)
+);
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+
+CREATE TABLE invoice_treatments (
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id),
+    PRIMARY KEY (invoice_id, treatment_id)
 );
